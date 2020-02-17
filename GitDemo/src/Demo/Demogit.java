2d72cc8e811da9cdaaa608d00e727d0794973425
AMITRAJE <AMITRAJE@AMITRAJE-PC>
2020-02-17 12:09:42 +0530

    M1 method create

--- GitDemo/src/Demo/Demogit.java
+++ GitDemo/src/Demo/Demogit.java
@@ -1,10 +1,16 @@
 package Demo;
 
-public class Demogit {
+public class Demogit 
+{
+	public void m1()
+	{
+		System.out.println("This m1 Method..");
+	}
+	
 
 	public static void main(String[] args) 
 	{
-		System.out.println("This is main Method"); dg = new Demogit();
+		System.out.println("This is main Method"); 
 		
 		Demogit dg = new Demogit();
 	}
